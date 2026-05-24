d2e8d6a7d4be8da6f6ac192fcab119b7175223f9
brianthest <brianndamage@gmail.com>
2026-05-24 00:46:40 -0700

    first commit

---
+++ assets/README.txt
@@ -0,0 +1,8 @@
+# assets/
+
+Place these files in this folder:
+
+- dog1.png   → puppy with cap and flowers (Page 1)
+- dog2.png   → chihuahua angry face (Page 2)
+- dog3.png   → dog with black hat and red rose (Page 3)
+- video.mp4  → your gift video (Page 4)
---
+++ main.js
@@ -0,0 +1,15 @@
+function goYes() {
+  window.location.href = 'page3.html';
+}
+
+function goNo() {
+  window.location.href = 'page2.html';
+}
+
+function tryAgain() {
+  window.location.href = 'page1.html';
+}
+
+function goNext() {
+  window.location.href = 'page4.html';
+}
